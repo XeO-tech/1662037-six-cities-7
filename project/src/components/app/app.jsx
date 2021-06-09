79f253c957fb210ab24b24d3153c4b21d742f2ba
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-06-09 11:55:03 +0300

    add main page

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import MainPage from '../main-page/main-page';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App(props) {
+  const {offersNumber} = props;
+
+  return <MainPage offersNumber = {offersNumber}/>;
 }
+
+App.propTypes = {
+  offersNumber: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  OFFERS_NUMBER: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App offersNumber = {Setting.OFFERS_NUMBER}/>
   </React.StrictMode>,
   document.getElementById('root'));
