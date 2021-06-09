acd09212b6731082971f57a819aac4217e1e2bc0
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-06-09 15:58:45 +0300

    add page components and routing

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,11 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import MainPage from '../main-page/main-page';
+import SignIn from '../sign-in/sign-in';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
+import NotFound from '../not-found/not-found';
 
 function App(props) {
   const {offersNumber} = props;
 
-  return <MainPage offersNumber = {offersNumber}/>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <MainPage offersNumber = {offersNumber}/>
+        </Route>
+        <Route exact path={AppRoute.LOGIN}>
+          <SignIn />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites />
+        </Route>
+        <Route exact path={AppRoute.ROOM}>
+          <Room />
+        </Route>
+        <Route>
+          <NotFound />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 App.propTypes = {
