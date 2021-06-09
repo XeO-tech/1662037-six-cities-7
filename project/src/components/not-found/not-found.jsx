e86f2424e88e4be9ec52f2177cc1b7f908ea01f8
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-06-09 16:06:36 +0300

    modify not-found component to look better

--- project/src/components/not-found/not-found.jsx
+++ project/src/components/not-found/not-found.jsx
@@ -2,9 +2,41 @@ import React from 'react';
 
 function NotFound() {
   return (
-    <div>
-      <h1>404. Page not found</h1>
-      <a href="/">Вернуться на главную</a>
+    <div className="page page--gray page--main">
+      <header className="header">
+        <div className="container">
+          <div className="header__wrapper">
+            <div className="header__left">
+              <a className="header__logo-link header__logo-link--active" href="foo">
+                <img className="header__logo" src="img/logo.svg" alt="6 cities logo" width="81" height="41" />
+              </a>
+            </div>
+            <nav className="header__nav">
+              <ul className="header__nav-list">
+                <li className="header__nav-item user">
+                  <a className="header__nav-link header__nav-link--profile" href="foo">
+                    <div className="header__avatar-wrapper user__avatar-wrapper">
+                    </div>
+                    <span className="header__user-name user__name">Oliver.conner@gmail.com</span>
+                  </a>
+                </li>
+                <li className="header__nav-item">
+                  <a className="header__nav-link" href="foo">
+                    <span className="header__signout">Sign out</span>
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </header>
+
+      <main className="page__main page__main--index">
+        <div className="page__favorites-container container">
+          <h1>404. Page not found</h1>
+          <a href="/">Вернуться на главную</a>
+        </div>
+      </main>
     </div>
   );
 }
