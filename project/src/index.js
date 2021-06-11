3a2c15eaa5b98f67bea86dc1bd40294c9415af02
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-06-11 19:41:56 +0300

    add adapter to client. WIP

--- project/src/index.js
+++ project/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
+import { offers } from './mocks/offers';
+import { reviews } from './mocks/reviews';
+import { adaptOfferToClient } from './utils';
 
 const Setting = {
   OFFERS_NUMBER: 5,
 };
+console.log(offers, offers.map((offer) => adaptOfferToClient(offer)));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App offersNumber = {Setting.OFFERS_NUMBER}/>
+    <App
+      offersNumber = {Setting.OFFERS_NUMBER}
+      offers = {offers}
+      reviews = {reviews}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
---
+++ project/src/utils.js
@@ -0,0 +1,26 @@
+export const adaptOfferToClient = (offer) => {
+  const adaptedOffer = Object.assign(
+    {},
+    offer,
+    {
+      //  host является вложенным объектом, пэтому не создается новый, а мутируется
+      // host: {
+      //   avatarUrl: offer.host['avatar_url'],
+      //   isPro: offer.host['is_pro'],
+      // },
+      isFavorite: offer['is_favorite'],
+      isPremium: offer['is_premium'],
+      maxAdults: offer['max_adults'],
+      previewImage: offer['preview_image'],
+    });
+
+  delete adaptedOffer.host['avatar_url'];
+  delete adaptedOffer.host['is_pro'];
+  delete adaptedOffer['is_favorite'];
+  delete adaptedOffer['is_premium'];
+  delete adaptedOffer['max_adults'];
+  delete adaptedOffer['preview_image'];
+
+  return adaptedOffer;
+};
+
