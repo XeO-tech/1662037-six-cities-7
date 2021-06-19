14f915f3109e56cb8706e9f6d330ef68d5c1f495
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-06-19 20:12:21 +0300

    fix main page to add option of rendering 0 offers

--- project/src/components/main-page/main-page.jsx
+++ project/src/components/main-page/main-page.jsx
@@ -6,6 +6,52 @@ import { cardProp } from '../card/card.prop';
 
 function MainPage(props) {
   const {offers} = props;
+
+  const emptyPage = (
+    <div className="cities__places-container cities__places-container--empty container">
+      <section className="cities__no-places">
+        <div className="cities__status-wrapper tabs__content">
+          <b className="cities__status">No places to stay available</b>
+          <p className="cities__status-description">We could not find any property available at the moment in Dusseldorf</p>
+        </div>
+      </section>
+      <div className="cities__right-section" />
+    </div>
+  );
+
+  const pageWithCards = (
+    <div className="cities__places-container container">
+      <section className="cities__places places">
+        <h2 className="visually-hidden">Places</h2>
+        <b className="places__found">312 places to stay in Amsterdam</b>
+        <form className="places__sorting" action="#" method="get">
+          <span className="places__sorting-caption">Sort by</span>
+          <span className="places__sorting-type" tabIndex="0">
+                  Popular
+            <svg className="places__sorting-arrow" width="7" height="4">
+              <use xlinkHref="#icon-arrow-select"></use>
+            </svg>
+          </span>
+          <ul className="places__options places__options--custom places__options--opened">
+            <li className="places__option places__option--active" tabIndex="0">Popular</li>
+            <li className="places__option" tabIndex="0">Price: low to high</li>
+            <li className="places__option" tabIndex="0">Price: high to low</li>
+            <li className="places__option" tabIndex="0">Top rated first</li>
+          </ul>
+        </form>
+        <div className="cities__places-list places__list tabs__content">
+          <OffersList
+            offers={offers}
+            isFavoritePage={false}
+          />
+        </div>
+      </section>
+      <div className="cities__right-section">
+        <section className="cities__map map"></section>
+      </div>
+    </div>
+  );
+
   return (
     <div className="page page--gray page--main">
       <header className="header">
@@ -35,7 +81,6 @@ function MainPage(props) {
           </div>
         </div>
       </header>
-
       <main className="page__main page__main--index">
         <h1 className="visually-hidden">Cities</h1>
         <div className="tabs">
@@ -75,36 +120,7 @@ function MainPage(props) {
           </section>
         </div>
         <div className="cities">
-          <div className="cities__places-container container">
-            <section className="cities__places places">
-              <h2 className="visually-hidden">Places</h2>
-              <b className="places__found">312 places to stay in Amsterdam</b>
-              <form className="places__sorting" action="#" method="get">
-                <span className="places__sorting-caption">Sort by</span>
-                <span className="places__sorting-type" tabIndex="0">
-                  Popular
-                  <svg className="places__sorting-arrow" width="7" height="4">
-                    <use xlinkHref="#icon-arrow-select"></use>
-                  </svg>
-                </span>
-                <ul className="places__options places__options--custom places__options--opened">
-                  <li className="places__option places__option--active" tabIndex="0">Popular</li>
-                  <li className="places__option" tabIndex="0">Price: low to high</li>
-                  <li className="places__option" tabIndex="0">Price: high to low</li>
-                  <li className="places__option" tabIndex="0">Top rated first</li>
-                </ul>
-              </form>
-              <div className="cities__places-list places__list tabs__content">
-                <OffersList
-                  offers={offers}
-                  isFavoritePage={false}
-                />
-              </div>
-            </section>
-            <div className="cities__right-section">
-              <section className="cities__map map"></section>
-            </div>
-          </div>
+          {offers.length === 0 ? emptyPage : pageWithCards}
         </div>
       </main>
     </div>
