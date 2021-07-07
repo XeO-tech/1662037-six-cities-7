8c768cdd3b5efec2e2edb3bb267efbafad3bac7d
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-07-07 16:57:13 +0300

    refactore actions to use createAction function from reduxjs tools

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -1,3 +1,5 @@
+import { createAction } from '@reduxjs/toolkit';
+
 export const ActionType = {
   CHANGE_CITY: 'app/changeCity',
   LOAD_OFFERS: 'data/offers',
@@ -6,26 +8,20 @@ export const ActionType = {
   REDIRECT_TO_ROUTE: 'app/redirect',
 };
 
-export const changeCity = (newCity) => ({
-  type: ActionType.CHANGE_CITY,
+export const changeCity = createAction(ActionType.CHANGE_CITY, (newCity) => ({
   payload: newCity,
-});
+}));
 
-export const loadOffers = (offers) => ({
-  type: ActionType.LOAD_OFFERS,
+export const loadOffers = createAction(ActionType.LOAD_OFFERS, (offers) => ({
   payload: offers,
-});
+}));
 
-export const requireAuthorization = (status) => ({
-  type: ActionType.REQUIRE_AUTHORIZATION,
+export const requireAuthorization = createAction(ActionType.REQUIRE_AUTHORIZATION, (status) => ({
   payload: status,
-});
+}));
 
-export const logout = () => ({
-  type: ActionType.LOGOUT,
-});
+export const logout = createAction(ActionType.LOGOUT);
 
-export const redirectToRoute = (url) => ({
-  type: ActionType.REDIRECT_TO_ROUTE,
+export const redirectToRoute = createAction(ActionType.REDIRECT_TO_ROUTE, (url) => ({
   payload: url,
-});
+}));
