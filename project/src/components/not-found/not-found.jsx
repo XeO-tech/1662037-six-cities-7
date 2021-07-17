b8833e05791e27f1ee2cc7241be434440a4aa65f
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-07-17 14:14:23 +0300

    add test to NotFound component

--- project/src/components/not-found/not-found.jsx
+++ project/src/components/not-found/not-found.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import Header from '../header/header';
 import { Link } from 'react-router-dom';
 import { AppRoute } from '../../const';
 
 function NotFound() {
   return (
     <div className="page page--gray page--main">
-      <Header />
       <main className="page__main page__main--index">
         <div className="page__favorites-container container">
           <h1>404. Page not found</h1>
