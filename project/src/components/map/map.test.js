38f9d002ceb31248ea149aa6e37f99d257aa6529
XeO-tech <75488901+XeO-tech@users.noreply.github.com>
2021-07-22 14:08:11 +0300

    add test to Map

---
+++ project/src/components/map/map.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import Map from './map';
+import { testOffers } from './map.test-mocks';
+
+
+describe('Component: Review', () => {
+  render(<Map offers={testOffers} activeCardId={null} currentCard={null}/>);
+
+  it('should render leaflet map', () => {
+
+    expect(document.querySelector('.leaflet-container')).toBeInTheDocument();
+  });
+
+  it('should render on map markers for each offer', () => {
+
+    expect(document.querySelectorAll('.leaflet-marker-icon').length).toEqual(3);
+  });
+});
+
